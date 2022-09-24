7d7958d34c2fa4d0fdf142c846502f6ffea03cd1
goutham <gouthamshine@gmail.com>
2022-09-25 00:49:06 +0100

    print all permutations of a given string - iterative

--- recursion/02-permutations.py
+++ recursion/02-permutations.py
@@ -1,5 +1,8 @@
-def permutation(string, pocket=''):
-    """Function to print all the permutations of a given string"""
+from math import factorial
+
+
+def permutation_recursive(string: str, pocket='') -> None:
+    """Recursive function to print all the permutations of a given string"""
     if len(string) == 0:
         print(pocket)
     else:
@@ -8,8 +11,25 @@ def permutation(string, pocket=''):
             head = string[0:i]
             tail = string[i+1:]
             together = head + tail
-            permutation(together, current_letter + pocket)
+            permutation_recursive(together, current_letter + pocket)
+
+
+def permutation_iterative(string: str) -> None:
+    for p in range(factorial(len(string))):
+        print(''.join(string))
+        i = len(string) - 1
+        while i > 0 and string[i - 1] > string[i]:
+            i -= 1
+        string[i:] = reversed(string[i:])
+        if i > 0:
+            q = i
+            while string[i - 1] > string[q]:
+                q += 1
+            temp = string[i - 1]
+            string[i - 1] = string[q]
+            string[q] = temp
 
 
 if __name__ == '__main__':
-    permutation('ABC')
+    # permutation_recursive('ABC')
+    permutation_iterative(list('ABC'))
